fafca1c02588c25a87109069ab389034dd1f93b0
Blake Smith <blakesmith0@gmail.com>
2011-04-25 22:57:36 -0500

    Don't establish the connection and context in each request

--- service/service.rb
+++ service/service.rb
@@ -15,8 +15,6 @@ end
 
 class Service < Goliath::API
   def response(env)
-    context = EM::ZeroMQ::Context.new(1)
-    req_socket = context.connect(ZMQ::REQ, "tcp://127.0.0.1:5555")
     req_socket.handler = Handler.new
 
     json = {:message => 'convert', :url => "http://localhost:4567/images/briones_yeah.jpg", :width => 60}.to_json
