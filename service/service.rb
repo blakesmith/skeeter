6a2a49ec5b38ac08166d15c01b75d7f196bb4403
Blake Smith <blakesmith0@gmail.com>
2011-04-26 07:51:10 -0500

    Refactor em handler to encapsulate more of the logic

--- service/service.rb
+++ service/service.rb
@@ -1,15 +1,26 @@
 require 'goliath'
 require 'json'
 
-class Handler
+class EM::Protocols::ZMQHandler
   attr_reader :received
 
-  def initialize
-    @f = Fiber.current
+  def initialize(connection)
+    @connection = connection
+    @client_fiber = Fiber.current
+    @connection.setsockopt(ZMQ::IDENTITY, "web-req#{@client_fiber.object_id}")
+    @connection.handler = self
+  end
+
+  def send_msg(msg)
+    queued = @connection.send_msg(msg)
+    @connection.register_readable
+    puts "Sending #{msg}" if queued
+    messages = Fiber.yield
+    messages.map(&:copy_out_string)
   end
 
   def on_readable(socket, messages)
-    @f.resume(messages)
+    @client_fiber.resume(messages)
   end
 end
 
@@ -19,14 +30,9 @@ class Service < Goliath::API
 
   def response(env)
     json = {:message => 'convert', :width => params['width'], :url => params['image_url']}.to_json
-    connection_pool.execute(false) do |socket|
-      socket.setsockopt(ZMQ::IDENTITY, "web-req#{Fiber.current.object_id}")
-      socket.handler = Handler.new
-      queued = socket.send_msg(json)
-      puts "Sending #{json}" if queued
-      socket.register_readable
-      messages = Fiber.yield
-      resp = messages.first.copy_out_string
+    connection_pool.execute(false) do |conn|
+      handler = EM::Protocols::ZMQHandler.new(conn)
+      resp = handler.send_msg(json).first
       [200, {}, resp]
     end
   end
