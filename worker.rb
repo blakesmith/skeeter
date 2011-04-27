68d1a20b12ba03ba4acaf2435533022af0693021
Blake Smith <blakesmith0@gmail.com>
2011-04-27 00:20:31 -0500

    Fork workers and properly shut them down

--- worker.rb
+++ worker.rb
@@ -3,26 +3,42 @@ require 'ffi-rzmq'
 require 'open-uri'
 require 'json'
 
-context = ZMQ::Context.new(1)
+WORKER_COUNT = ARGV[0].to_i || 10
 
-receiver = context.socket(ZMQ::REP)
-receiver.connect("ipc://dispatch-back.ipc")
+pids = []
 
 def fetch_image(url, width)
   response = `jp2a --width=#{width} "#{url}"`
 end
 
-while true
-  str = receiver.recv_string
+WORKER_COUNT.times do |i|
+  pids << fork do
+    trap("INT") { exit }
+    puts "Starting worker #{i}..."
+    context = ZMQ::Context.new(1)
 
-  message = JSON.parse(str)
-  puts "Got message #{message.inspect}"
+    receiver = context.socket(ZMQ::REP)
+    receiver.connect("ipc://dispatch-back.ipc")
 
-  # Do some work
-  if message['message'] = "convert"
-    width = message['width'] || 80
-    image = fetch_image(message['url'], width)
-    receiver.send_string(image)
+    loop do
+      str = receiver.recv_string
+
+      message = JSON.parse(str)
+      puts "Worker #{i}: Got message #{message.inspect}"
+
+      # Do some work
+      if message['message'] = "convert"
+        width = message['width'] || 80
+        image = fetch_image(message['url'], width)
+        receiver.send_string(image)
+      end
+    end
   end
 end
 
+trap("INT") do
+  puts "Killing all workers..."
+  pids.map {|p| Process.kill("INT", p) }
+end
+
+Process.wait
