7e7f70cb7411f3fc0abbc35f7e0e57af19cb0abb
Blake Smith <blakesmith0@gmail.com>
2011-04-29 23:10:11 -0500

    Skeeter goliath controller

---
+++ skeeter_controller.rb
@@ -0,0 +1,23 @@
+def start
+  puts "Starting Skeeter..."
+  `ruby service/skeeter.rb -d -e prod`
+end
+
+def stop
+  puts "Killing Skeeter..."
+  pid_path = File.join(File.dirname(__FILE__), 'service', 'goliath.pid')
+  pid = File.read(pid_path).to_i
+  status = Process.kill("TERM", pid)
+  puts "Killed pid #{pid}" if status == 1
+end
+
+if ARGV[0] == "start"
+  start
+elsif ARGV[0] == "stop"
+  stop
+elsif ARGV[0] == "restart"
+  stop
+  start
+else
+  puts "Unknown command"
+end
