03c5b58b48864c22548373d0f25646241c1224c6
Blake Smith <blakesmith0@gmail.com>
2011-04-30 02:13:35 -0500

    Update skeeter pid locations

--- skeeter_controller.rb
+++ skeeter_controller.rb
@@ -1,11 +1,11 @@
 def start
   puts "Starting Skeeter..."
-  `ruby service/skeeter.rb -d -e prod -v`
+  `ruby service/skeeter.rb -d -e prod -v -p tmp/pids/skeeter.pid`
 end
 
 def stop
   puts "Killing Skeeter..."
-  pid_path = File.join(File.dirname(__FILE__), 'service', 'goliath.pid')
+  pid_path = File.join(File.dirname(__FILE__), 'tmp', 'pids', 'skeeter.pid')
   pid = File.read(pid_path).to_i
   status = Process.kill("TERM", pid)
   puts "Killed pid #{pid}" if status == 1
