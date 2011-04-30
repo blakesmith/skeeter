3c0a1a392c8a62cbaca0ccb3dba4a9bc0dd94e16
Blake Smith <blakesmith0@gmail.com>
2011-04-29 23:59:30 -0500

    Capistrano deploy scripts

---
+++ config/deploy.rb
@@ -0,0 +1,80 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, "1.9.2-p180"
+set :rvm_type, :user  # Copy the exact line. I really mean :user here
+
+set :user, "rails"
+set :primary_server, "stiletto.blakesmith.me"
+
+set :application, "skeeter"
+set :repository,  "ssh://#{user}@#{primary_server}/home/blake/pubgit/#{application}.git"
+
+set :scm, "git"
+set :branch, "master"
+set :deploy_to, "/home/rails/#{application}"
+set :deploy_via, :remote_cache
+
+set :runner, nil
+
+role :app, "#{primary_server}"                          # This may be the same as your `Web` server
+
+before('deploy:cleanup') { set :use_sudo, false }
+before('deploy:setup') { set :use_sudo, false }
+after 'deploy:update_code', 'bundler:bundle_new_release'
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+#    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+
+namespace :deploy do
+  task :start_dispatcher do
+    run "cd #{deploy_to}/current && ruby dispatcher_controller.rb start"
+  end
+
+  task :start_workers do
+    run "cd #{deploy_to}/current && ruby worker_controller.rb start"
+  end
+
+  task :start_server do
+    run "cd #{deploy_to}/current && ruby skeeter_controller.rb start"
+  end
+
+  task :stop_dispatcher do
+    run "cd #{deploy_to}/current && ruby dispatcher_controller.rb stop"
+  end
+
+  task :stop_workers do
+    run "cd #{deploy_to}/current && ruby worker_controller.rb stop"
+  end
+
+  task :stop_server do
+    run "cd #{deploy_to}/current && ruby skeeter_controller.rb stop"
+  end
+
+  task :start do
+    start_dispatcher
+    start_workers
+    start_server
+  end
+
+  task :stop do 
+    stop_server
+    stop_workers
+    stop_dispatcher
+  end
+
+  task :restart do
+    stop
+    start
+  end
+end
